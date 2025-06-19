be44e8d25a5ef819ce0bf65df55dfa5dca01cd38
K-Mirembe-Mercy <133584067+K-Mirembe-Mercy@users.noreply.github.com>
2025-06-19 09:36:50 -0700

    Add files via upload

---
+++ exercise4,2.py
@@ -0,0 +1,9 @@
+numbers = list(range(1, 1000000))
+min(numbers)
+max(numbers)
+sum(numbers)
+print(numbers)
+# exercise 4,4
+odd_numbers = list(range(1, 20, 2))
+for i in odd_numbers:
+    print(odd_numbers)
